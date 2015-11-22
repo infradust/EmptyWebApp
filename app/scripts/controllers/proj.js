ef6f0c52df8696082faa2a52cbef7103346c173e
Dan Shelly <danshelly@gmail.com>
2015-11-22 15:06:18 +0200

    Average comparison

--- app/scripts/controllers/proj.js
+++ app/scripts/controllers/proj.js
@@ -10,8 +10,17 @@
 angular.module('testYoApp')
   .controller('ProjCtrl', ['$scope','demoData',function ($scope,demoData) {
   	var self = this;
-  	var bias = {travel:60,travelTime:60,placeAndRelease:-5*60};
-	self.proj = $scope.proj = demoData.simulateProject(3,bias);	  
+  	var bias = {travel:60,travelTime:2*60,placeAndRelease:5*60};
+	self.proj = $scope.proj = demoData.simulateProject(7,bias);	  
 	$scope.visDlg = self;
+	$scope.day = 0;
+	self.$scope=$scope;
 	console.log('PROJ:',$scope.proj);
+	
+	$scope.changeDay = function () {
+		$scope.day++;
+		if ($scope.day > $scope.proj.days.length-1) {
+			$scope.day = 0;	
+		}
+	};
 }]);
--- app/scripts/directives/projd3stats.js
+++ app/scripts/directives/projd3stats.js
@@ -37,139 +37,159 @@ angular.module('testYoApp')
 			 .on("zoom", z);
 		ctrlGroup.call(zoomer);
 		
-		var data = [{t:demoData.projectExpectedSchedule.d0.expected,k:'expected'},{t:dlg.proj.days[0].total,k:'actual'}];
-		
 		var x_group = d3.scale.ordinal().rangeRoundBands([0,w],0.1);
 		var y = d3.scale.linear().range([h+margin.top,margin.top]);
-		
 		var xAxis = d3.svg.axis().scale(x_group).orient('bottom');
 		var yAxis = d3.svg.axis().scale(y).orient('left').tickFormat(d3.format('.2s'));
+		function update(day) {
+			content.html('');
+			console.log('DAY '+day,dlg.proj.days[day].total);
+			var data = [{t:demoData.projectExpectedSchedule['d'+day].expected,k:'expected'},{t:dlg.proj.days[day].total,k:'actual'}];
+			var expPlateCount = demoData.projectExpectedSchedule['d'+day].plates;
+			var actPlateCount = dlg.proj.days[day].total.plateProcess.plates;
 		
-		var processedData = [];
-		var maxY = 0;
-		var maxs = {
-			connect:0,
-			connectY:0,
-			travel:0,
-			travelY:0,
-			placeAndRelease:0,
-			placeAndReleaseY:0,
-			travelTime:0,
-			tavelTimeY:0,
-		};
-		data.forEach(function(d){
-			var vals = d.t.plateProcess;
-			var pd = {
-				plate:{
-					d:d.k,
-					k:'plate',
-					stack:[
-						{y0:0,y1:(vals.connect)/60,k:'connect',d:d.k},
-						{y0:(vals.connect)/60,y1:(vals.connect+vals.travel)/60,k:'travel',d:d.k},
-						{y0:(vals.connect+vals.travel)/60,y1:(vals.connect+vals.travel+vals.placeAndRelease)/60,k:'placeAndRelease',d:d.k}
-					],
-				},
-				reaquire:{
-					d:d.k,
-					k:'reaquire',
-					stack:[
-						{y0:0,y1:d.t.reaquire.travelTime/60,k:'travelTime',d:d.k},
-					]
-				},
+			var processedData = [];
+			var maxY = 0;
+/*
+			var maxs = {
+				connect:0,
+				connectY:0,
+				travel:0,
+				travelY:0,
+				placeAndRelease:0,
+				placeAndReleaseY:0,
+				travelTime:0,
+				tavelTimeY:0,
 			};
-			var st = pd.plate.stack[0];
-			if (maxs.connect<(st.y1-st.y0)) {
-				maxs.connect = (st.y1-st.y0);
-				maxs.connectY = st.y1;
-			}
-			st = pd.plate.stack[1];
-			if (maxs.travel<(st.y1-st.y0)) {
-				maxs.travel = (st.y1-st.y0);
-				maxs.travelY = st.y1;
-			}
-			st = pd.plate.stack[2];
-			if (maxs.placeAndRelease<(st.y1-st.y0)) {
-				maxs.placeAndRelease = (st.y1-st.y0);
-				maxs.placeAndReleaseY = st.y1;
-			}
-			st = pd.reaquire.stack[0];
-			if (maxs.travelTime<st.y1-st.y0) {
-				maxs.travelTime = st.y1-st.y0;
-				maxs.travelTimeY = st.y1;
-			}
+*/
+			data.forEach(function(d){
+				var fact = (d.k == 'actual' ? 1/(60*actPlateCount):1/(60*expPlateCount));
+				var vals = d.t.plateProcess;
+				var vc = vals.connect*fact;
+				var vt = vals.travel*fact;
+				var vp = vals.placeAndRelease*fact;
+				var vtt = d.t.reaquire.travelTime*fact;
+				var pd = {
+					plate:{
+						d:d.k,
+						k:'plate',
+						stack:[
+							{y0:0,y1:(vc),k:'connect',d:d.k},
+							{y0:vc,y1:(vc+vt),k:'travel',d:d.k},
+							{y0:(vc+vt),y1:(vc+vt+vp),k:'placeAndRelease',d:d.k}
+						],
+					},
+					reaquire:{
+						d:d.k,
+						k:'reaquire',
+						stack:[
+							{y0:0,y1:vtt,k:'travelTime',d:d.k},
+						]
+					},
+				};
+/*
+				var st = pd.plate.stack[0];
+				if (maxs.connect<(st.y1-st.y0)) {
+					maxs.connect = (st.y1-st.y0);
+					maxs.connectY = st.y1;
+				}
+				st = pd.plate.stack[1];
+				if (maxs.travel<(st.y1-st.y0)) {
+					maxs.travel = (st.y1-st.y0);
+					maxs.travelY = st.y1;
+				}
+				st = pd.plate.stack[2];
+				if (maxs.placeAndRelease<(st.y1-st.y0)) {
+					maxs.placeAndRelease = (st.y1-st.y0);
+					maxs.placeAndReleaseY = st.y1;
+				}
+				st = pd.reaquire.stack[0];
+				if (maxs.travelTime<st.y1-st.y0) {
+					maxs.travelTime = st.y1-st.y0;
+					maxs.travelTimeY = st.y1;
+				}
+*/
+				
+	
+				if (pd.plate.stack[2].y1 > maxY) {
+					maxY = pd.plate.stack[2].y1;
+				}
+				if (pd.reaquire.stack[0].y1 > maxY) {
+					maxY = pd.reaquire.stack[0].y1;
+				}
+	
+				processedData.push(pd.plate);
+				processedData.push(pd.reaquire);
+			});
+			//maxY = maxY/60;
+			x_group.domain(['plate','reaquire']);
+			var x_comp = d3.scale.ordinal().domain(['expected','actual']).rangeRoundBands([0,x_group.rangeBand()]);
+			y.domain([0,maxY]);
 			
+			var color = {
+				connect:['red','Connect'],
+				travel:['green','Lift'],
+				placeAndRelease:['blue','Place & Release'],
+				travelTime:['orange','Reacquire'],
+			};
+	
+			var xplane = content.append('svg:g')
+			  .attr('class', 'x axis')
+			  .attr('transform', 'translate(0,' + (h+margin.top) + ')')
+			  .call(xAxis);
+			
+			content.append('svg:g')
+			  .attr('class', 'y axis')
+			  .call(yAxis)
+			.append('text')
+			  .attr('transform', 'rotate(-90)')
+			  .attr('y', 6)
+			  .attr('dy', '.71em')
+			  .style('text-anchor', 'end')
+			  .text('Time[min]');
+			
+			var event = content.selectAll('.event').data(processedData)
+				.enter().append('svg:g')
+					.classed('event',true)
+					.attr('transform',function(d){return 'translate('+x_group(d.k)+',0)';});
+				
+			event.selectAll('rect').data(function(d){return d.stack;})
+				.enter().append('rect')
+					.attr('x',function(d){return x_comp(d.d);})
+					.attr('width',x_group.rangeBand()/4)
+					.attr('y',function(d){return y(d.y1);})
+					.attr('height',function(d){return y(d.y0)-y(d.y1);})
+					.attr('fill',function(d){return color[d.k][0];});
+			event.append('text').attr('x',function(d){return x_comp(d.d);}).attr('y',20).text(function(d){return ''+d.d;});
+			
+			 var legend = content.selectAll(".legend")
+			 .data(Object.keys(color))
+			 .enter().append("g")
+			 	.attr("class", "legend")
+			 	.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+	
+			legend.append("rect")
+				.attr("x", w - 18)
+				.attr("width", 18)
+				.attr("height", 18)
+				.style("fill", function(d){return color[d][0];});
+			
+			legend.append("text")
+				.attr("x", w - 24)
+				.attr("y", 9)
+				.attr("dy", ".35em")
+				.style("text-anchor", "end")
+				.text(function(d) { return color[d][1]; });
 
-			if ((vals.connect+vals.travel+vals.placeAndRelease) > maxY) {
-				maxY = (vals.connect+vals.travel+vals.placeAndRelease);
+		}
+		
+		update(dlg.$scope.day);
+		
+		scope.$watch('dlg.$scope.day',function(day){
+			if (day !== undefined) {
+				update(day);
 			}
-			if (d.t.reaquire.travelTime > maxY) {
-				maxY = d.t.reaquire.travelTime;
-			}
-
-			processedData.push(pd.plate);
-			processedData.push(pd.reaquire);
 		});
-//		maxY = maxs.connect+maxs.travel+maxs.placeAndRelease;
-		maxY = maxY/60;
-		x_group.domain(['plate','reaquire']);
-		var x_comp = d3.scale.ordinal().domain(['expected','actual']).rangeRoundBands([0,x_group.rangeBand()]);
-		y.domain([0,maxY]);
-		
-		console.log('y(0):',y(0),'y(maxY)',y(maxY));
-		console.log('PDATA:',processedData[0].stack.map(function(d){return [y(d.y0),y(d.y1)];}));
-		var color = {
-			connect:['red','Connect'],
-			travel:['green','Lift'],
-			placeAndRelease:['blue','Place & Release'],
-			travelTime:['orange','Reacquire'],
-		};
-
-		var xplane = content.append('svg:g')
-		  .attr('class', 'x axis')
-		  .attr('transform', 'translate(0,' + (h+margin.top) + ')')
-		  .call(xAxis);
-		
-		content.append('svg:g')
-		  .attr('class', 'y axis')
-		  .call(yAxis)
-		.append('text')
-		  .attr('transform', 'rotate(-90)')
-		  .attr('y', 6)
-		  .attr('dy', '.71em')
-		  .style('text-anchor', 'end')
-		  .text('Time[min]');
-		
-		var event = content.selectAll('.event').data(processedData)
-			.enter().append('svg:g')
-				.classed('event',true)
-				.attr('transform',function(d){return 'translate('+x_group(d.k)+',0)';});
-			
-		event.selectAll('rect').data(function(d){return d.stack;})
-			.enter().append('rect')
-				.attr('x',function(d){return x_comp(d.d);})
-				.attr('width',x_group.rangeBand()/4)
-				.attr('y',function(d){return y(d.y1);})
-				.attr('height',function(d){return y(d.y0)-y(d.y1);})
-				.attr('fill',function(d){return color[d.k][0];});
-		
-		 var legend = content.selectAll(".legend")
-		 .data(Object.keys(color))
-		 .enter().append("g")
-		 	.attr("class", "legend")
-		 	.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-		legend.append("rect")
-			.attr("x", w - 18)
-			.attr("width", 18)
-			.attr("height", 18)
-			.style("fill", function(d){return color[d][0];});
-		
-		legend.append("text")
-			.attr("x", w - 24)
-			.attr("y", 9)
-			.attr("dy", ".35em")
-			.style("text-anchor", "end")
-			.text(function(d) { return color[d][1]; });
 
       }
     };
